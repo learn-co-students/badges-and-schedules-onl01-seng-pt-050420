dd0394bf6a038d3e1a1de50e8d7f7359a2d16d33
Victor Williams <victor.williams1030@gmail.com>
2020-05-11 20:17:58 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,18 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  badge_message = []
+  attendees.each { |name| badge_message.push(badge_maker(name)) }
+  return badge_message
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index { |name, index| room_assignments.push("Hello, #{name}! You'll be assigned to room #{index +1}!") }
+  return room_assignments
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each { |block| puts block.chomp }
+  assign_rooms(attendees).each { |block| puts block.chomp }
 end
