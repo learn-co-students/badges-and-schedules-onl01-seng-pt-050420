88f0292d8c3e4071444aa6a3449ae919777eced0
Angel Zuniga <angelzuniga@outlook.com>
2020-05-16 23:05:36 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,8 +11,8 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map do |speaker, index|
-    "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+  attendees.each_with_index.map do |speaker, room|
+    "Hello, #{speaker}! You'll be assigned to room #{room + 1}!"
   end
 end
 
